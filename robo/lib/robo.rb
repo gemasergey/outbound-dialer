64df3944b72329d3b3c1329fc31b0e71b437b57a
Sergei Gema <gs@rivas.ua>
2016-03-02 09:35:42 +0200

    initial commit

---
+++ robo/lib/robo.rb
@@ -0,0 +1,4 @@
+Robo = Module.new
+require "robo/version"
+require "robo/plugin"
+require "robo/controller_methods"
---
+++ robo/lib/robo/controller_methods.rb
@@ -0,0 +1,10 @@
+module Robo
+  module ControllerMethods
+    # The methods are defined in a normal method the user will then mix in their CallControllers
+    # The following also contains an example of configuration usage
+    #
+    def greet(name)
+      play "#{Adhearsion.config[:robo].greeting}, #{name}"
+    end
+  end
+end
